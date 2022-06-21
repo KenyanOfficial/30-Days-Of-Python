a1b761012dc3663e2930968c5dae7c625a0f75fe
Krakinz <103070588+Krakinz@users.noreply.github.com>
2022-06-22 00:09:51 +0300

    Update helloworld.py

--- 01_Day_Introduction/helloworld.py
+++ 01_Day_Introduction/helloworld.py
@@ -14,8 +14,11 @@ print(3 // 2)  # Floor division operator(//)
 print(type(10))                  # Int
 print(type(3.14))                # Float
 print(type(1 + 3j))              # Complex
-print(type('Asabeneh'))          # String
+print(type('Hello World'))       # String
 print(type([1, 2, 3]))           # List
 print(type({'name':'Asabeneh'})) # Dictionary
 print(type({9.8, 3.14, 2.7}))    # Set
 print(type((9.8, 3.14, 2.7)))    # Tuple
+
+#simple way
+print("hello world"
